09f971d54a3d9f68bd1afa9bee35b7be2ebb03df
Cristian Mateus <120053460+CMatheus7@users.noreply.github.com>
2024-11-23 19:01:23 -0300

    Update App.py

--- AppSt/App.py
+++ AppSt/App.py
@@ -9,7 +9,7 @@ st.sidebar.header("league")
 selected_league = st.sidebar.selectbox("league", ["Premier League", "La Liga", "Bundesliga", "Serie A", "Ligue 1"])
 
 st.sidebar.header("season")
-selected_season = st.sidebar.selectbox("Season", ["2013/2014", "2014/2015", "2015/2016", "2016/2017", "2017/2018", "2018/2019", "2019/2020", "2020/2021", "2021/2022", "2022/2023", "2023/2024, "2024/2025"])
+selected_season = st.sidebar.selectbox("Season", ["2013/2014", "2014/2015", "2015/2016", "2016/2017", "2017/2018", "2018/2019", "2019/2020", "2020/2021", "2021/2022", "2022/2023", "2023/2024", "2024/2025"])
 
 # Web scraping Footbal Data
 
