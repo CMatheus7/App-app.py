ba3c5de92b870cfa68f0652745cbe9d980141d33
Cristian Mateus <120053460+CMatheus7@users.noreply.github.com>
2024-11-23 20:47:48 -0300

    Update App.py

--- AppSt/App.py
+++ AppSt/App.py
@@ -1,15 +1,18 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+# Importando as bibliotecas necessárias
+import streamlit as st  # Streamlit para criar a interface web
+import pandas as pd  # Pandas para manipulação de dados
+import numpy as np  # Numpy para operações numéricas (não utilizado diretamente, mas importado)
 
-
+# Título do aplicativo Streamlit
 st.title("FOOTBALL DATA")
 
+# Adicionando a barra lateral com as opções para o usuário escolher a liga
 st.sidebar.header("league")
 selected_league = st.sidebar.selectbox(
     "league", ["England", "Germany", "Italy", "Spain", "France"]
 )
 
+# Adicionando a barra lateral com as opções para o usuário escolher a temporada
 st.sidebar.header("season")
 selected_season = st.sidebar.selectbox(
     "Season",
@@ -30,20 +33,21 @@ selected_season = st.sidebar.selectbox(
 )
 
 
-# Web scraping Footbal Data
+# Função para carregar os dados de futebol via web scraping
 def load_data(league, season):
-
+    # Mapeando o nome da liga selecionada para o código correspondente
     if selected_league == "England":
-        league = "E0"
+        league = "E0"  # Código para a liga da Inglaterra
     if selected_league == "Germany":
-        league = "D1"
+        league = "D1"  # Código para a liga da Alemanha
     if selected_league == "Italy":
-        league = "I1"
+        league = "I1"  # Código para a liga da Itália
     if selected_league == "Spain":
-        league = "SP1"
+        league = "SP1"  # Código para a liga da Espanha
     if selected_league == "France":
-        league = "F1"
+        league = "F1"  # Código para a liga da França
 
+    # Mapeando o nome da temporada para o formato necessário
     if selected_season == "2012/2013":
         season = "1213"
     if selected_season == "2013/2014":
@@ -71,14 +75,19 @@ def load_data(league, season):
     if selected_season == "2024/2025":
         season = "2425"
 
+    # Construindo a URL para acessar o arquivo CSV com os dados
     url = "https://www.football-data.co.uk/mmz4281/" + season + "/" + league + ".csv"
 
+    # Carregando os dados CSV a partir da URL
     data = pd.read_csv(url)
     return data
 
 
+# Carregando os dados para o campeonato e temporada selecionados
 df = load_data(selected_league, selected_season)
 
-
+# Exibindo o título da seção de dados com o nome da liga selecionada
 st.subheader("Dataframe: " + selected_league)
+
+# Exibindo o dataframe carregado na interface Streamlit
 st.dataframe(df)
