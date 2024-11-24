4af2bb53b5cbe3fe3978802ad84f9aae1bba651c
Cristian Mateus <120053460+CMatheus7@users.noreply.github.com>
2024-11-24 01:17:06 -0300

    Update jogos.py

--- AppSt/pages/jogos.py
+++ AppSt/pages/jogos.py
@@ -13,7 +13,7 @@ dia = st.date_input("Data de analise", date.today())
 
 def load_data_jogos():
     data_jogos = pd.read_csv(
-        "https://github.com/CMatheus7/Jogos_do_Dia_FlashScore/blob/main/jogos_"
+        "https://github.com/CMatheus7/Jogos_do_Dia_FlashScore/blob/main/jogos"
         + str(dia)
         + ".csv?raw=true"
     )
