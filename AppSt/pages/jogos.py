d600c259692f25aca5df51e00010b160de2600a9
Cristian Mateus <120053460+CMatheus7@users.noreply.github.com>
2024-11-23 21:01:10 -0300

    Update jogos.py

--- AppSt/pages/jogos.py
+++ AppSt/pages/jogos.py
@@ -4,6 +4,9 @@ import numpy as np
 import datetime
 from datetime import date
 
-
 # Título do aplicativo Streamlit
 st.title("Jogos do dia")
+
+
+dia = st.date_input("Data de analise") date.today())
+
